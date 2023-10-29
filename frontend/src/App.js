10a7e903e698202268f9015205ae768cae56256b
jalag17 <84992618+jalag17@users.noreply.github.com>
2023-10-29 13:53:32 -0700

    Jay's initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Nishant's commit</p>
+        <p>Jay's commit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
