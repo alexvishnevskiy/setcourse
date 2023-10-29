3f899a52b5863562376767ccc04e04242a2fda52
PeterSimm <148409200+PeterSimm@users.noreply.github.com>
2023-10-29 14:02:57 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React before Week 7.
         </a>
       </header>
     </div>
