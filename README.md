0bea9ebb3d83697d71c15a41045c2c94247d7438
alexvishnevskiy <mrflowy043@gmail.com>
2023-11-01 21:08:13 -0700

    Add database initialization

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # setcourse 
+
+## How to connect to Database?
+
+1. You can run using MySQL server with credentials from docker-compose.yml
+2. You can download docker, start docker server and then run docker-compose up. It will set up everything you need.
--- backend/app/configuration.py
+++ backend/app/configuration.py
@@ -4,12 +4,7 @@ class Config(object):
 	"""
 	DEBUG = False
 	TESTING = False
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///application.db'
-	BOOTSTRAP_FONTAWESOME = True
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:alex@localhost/setcourse'
 	SECRET_KEY = "MINHACHAVESECRETA"
 	CSRF_ENABLED = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-	#Get your reCaptche key on: https://www.google.com/recaptcha/admin/create
-	#RECAPTCHA_PUBLIC_KEY = "6LffFNwSAAAAAFcWVy__EnOCsNZcG2fVHFjTBvRP"
-	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
