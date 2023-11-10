db2fbd6bc848589e77313cb3cdf2048a3f5d44ef
alexvishnevskiy <mrflowy043@gmail.com>
2023-11-10 11:48:30 -0800

    Add docker-compose for all components

--- backend/app/configuration.py
+++ backend/app/configuration.py
@@ -4,7 +4,7 @@ class Config(object):
 	"""
 	DEBUG = True
 	TESTING = False
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:alex@localhost/setcourse'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:alex@mysql/setcourse' # use this one locally 'mysql+pymysql://alex:alex@localhost/setcourse'
 	SECRET_KEY = "MINHACHAVESECRETA"
 	CSRF_ENABLED = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
