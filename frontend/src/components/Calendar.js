06bb2b4a9184824acf8c2d785ab7e193f85d21fd
nishant misal <nishantmisal07@gmail.com>
2023-11-09 23:27:19 -0800

    More UI Updates

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
-import '../Calendar.css';
+import './Calendar.css';
 
-const Calendar = ({ config, courses}) => {
+const Calendar = ({ config, courses }) => {
     const determineStartPixel = (time) => {
         const date = new Date(time);
         const hour = date.getHours();
@@ -23,24 +23,23 @@ const Calendar = ({ config, courses}) => {
         3: "#E9E9E9", 
         4: "#d2c9a5", 
         5: "#c77b58"
-    };
-
-    const adjustEachCourse = (events) => {
-        const courseList = document.querySelectorAll(".calendar_default_event");
-        for(let i = 0; i < courseList.length; i++){
-            const courseID = courseList[i].event.id();
-            const courseObj = events.find((course) => course.id === courseID);
-            const start = courseObj.start.value;
-            const end = courseObj.end.value;
-            const startPixel = determineStartPixel(start);
-            const heightInPixels = determineHeightInPixels(start, end);
-            courseList[i].style.setProperty("top", `${startPixel}px`, "important");
-            courseList[i].style.setProperty("height", `${heightInPixels}px`, "important");
-            courseList[i].children[0].style.setProperty("background", `${colors[courseID]}`, "important");
-        }
-    }    
+    };  
 
     useEffect(() => {
+        const adjustEachCourse = (events) => {
+            const courseList = document.querySelectorAll(".calendar_default_event");
+            for(let i = 0; i < courseList.length; i++){
+                const courseID = courseList[i].event.id();
+                const courseObj = events.find((course) => course.id === courseID);
+                const start = courseObj.start.value;
+                const end = courseObj.end.value;
+                const startPixel = determineStartPixel(start);
+                const heightInPixels = determineHeightInPixels(start, end);
+                courseList[i].style.setProperty("top", `${startPixel}px`, "important");
+                courseList[i].style.setProperty("height", `${heightInPixels}px`, "important");
+                courseList[i].children[0].style.setProperty("background", `${colors[courseID]}`, "important");
+            }
+        }  
         adjustEachCourse(courses.events);
     }, [config, courses]);
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import Button from '../Button';
+import Button from '../components/Button';
 import Calendar from '../components/Calendar';
 import Header from '../components/Header'; 
 import Sidebar from '../components/Sidebar'; 
