70590247b662834588b7d19cb3aa2cefb2cf1714
alexvishnevskiy <mrflowy043@gmail.com>
2023-11-16 13:48:29 -0800

    Old docker-compose

--- backend/app/configuration.py
+++ backend/app/configuration.py
@@ -4,7 +4,7 @@ class Config(object):
 	"""
 	DEBUG = True
 	TESTING = False 
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:alex@mysql/setcourse'   # use this one for local 'mysql+pymysql://alex:alex@localhost/setcourse'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alex:alex@localhost/setcourse'  # use this one for docker  'mysql+pymysql://alex:alex@mysql/setcourse'
 	SECRET_KEY = "MINHACHAVESECRETA"
 	CSRF_ENABLED = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
